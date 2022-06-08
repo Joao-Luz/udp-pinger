494586319fe5043843b8a62b7f6a3b8194d93bc7
Joao-Luz <jlllima5@gmail.com>
2022-06-07 22:30:31 -0300

    Simulating more faults on the server side

--- udp_pinger_server.py
+++ udp_pinger_server.py
@@ -1,25 +1,87 @@
-from socket import *
-
 import random
+from socket import *
+import time
 import sys
 
 if len(sys.argv) < 2:
     print('Usage: python server.py <port>')
     sys.exit()
 
+ip = '127.0.0.0'
 port = int(sys.argv[1])
-ip = ''
 
-serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind((ip, port))
+server_socket = socket(AF_INET, SOCK_DGRAM)
+server_socket.bind((ip, port))
 
+simulate_packet_loss = True
+sleep_for_rand_response_times = True
+simulate_protocol_error = True
+simulate_delay_send = True
 
+def get_time():
+    return int(round(time.time() * 1000))
+
+msg = None
+addr = None
+delay = False
 while True:
-    message, address = serverSocket.recvfrom(30)
-    message = message.upper()
-    print(f'Recieve: {message}')
 
-    if random.random() < 0.1:
-        continue
+    try:
+        message, address = server_socket.recvfrom(1024)
+        message = message.decode('utf-8')
 
-    serverSocket.sendto(message, address)
+        id = message[0:5]
+        ping = message[5:6]
+        timestamp = message[6:10]
+        mssg = message[10:40]
+
+        if simulate_packet_loss and int(id) != 8 and int(id) != 9:
+            if random.random() < 0.1:
+                print(message)
+                print('Simulating packet loss')
+                continue
+
+        print(f'RECEIVED: {message}')
+
+        if ping == '0':
+            ping = '1'
+        else:
+            server_socket.sendto(message.encode('utf-8'), address)
+            print('Dropped: ping/pong error.')
+            continue
+        
+        # protocol error
+        if simulate_protocol_error and int(id) != 8:
+            if random.random() < 0.2:
+                pass
+            elif random.random() < 0.2:
+                ping = 'A'
+                print('Simulating Ping/Pong error')
+            elif random.random() < 0.2:
+                timestamp = '0000'
+                print('Simulating Timestamp error')
+
+        print(f'id: {len(id)}/{id}  ping: {len(ping)}/{ping} timestamp: {len(timestamp)}/{timestamp} msg: {len(mssg)}/{mssg}')
+
+        message = id + ping + timestamp + mssg
+
+        # delay
+        if int(id) < 5 and not delay and simulate_delay_send:
+            if random.randint(0, 10) < 3:
+                msg = message
+                addr = address
+                delay = True
+                print('Simulating packet delay ')
+                continue
+        
+        # sending package
+        if int(id) == 8 and delay and simulate_delay_send:
+            server_socket.sendto(msg.encode('utf-8'), addr)
+            print(f'Sending packet {msg}')
+            time.sleep(0.2)
+
+        server_socket.sendto(message.encode('utf-8'), address)
+        print(f'Send: {message} ')
+
+    except error:
+        print(f'Error {error}')
